5e8f8bf5b7a0e8176a79e0950e34d350fda28201
Adrianna B <pandziotron@outlook.com>
2021-11-07 09:49:20 +0100

    k

--- sda-csharp-exercises/Program.cs
+++ sda-csharp-exercises/Program.cs
@@ -4,9 +4,25 @@ namespace sda_csharp_exercises
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Witaj EOO :-)! ");
+            {
+                Cwiczenie1();
+            }
+
+            static void Cwiczenie1()
+            {
+                Console.Write("Podaj cenę produktu: ");
+                double price = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Podaj liczbę linii: ");
+                int linesCount = Convert.ToInt32(Console.ReadLine());
+
+                for (int i = 1; i <= linesCount; i++)
+                {
+                    Console.WriteLine($"Liczba sztuk: {i} łączna cena: {price * i}zł");
+                }
+            }
         }
     }
 }
